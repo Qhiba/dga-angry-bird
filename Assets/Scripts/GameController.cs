5ed378e32373c5dca97ce796ba0db784987e07d4
Qhiba <akbarhida@gmail.com>
2021-11-03 22:05:16 +0700

    First Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter slingShooter;
+    public TrailController trailController;
+    public List<Bird> birds;
+    public List<Enemy> enemies;
+
+    private Bird _shotBird;
+    public BoxCollider2D tapCollider;
+
+    [Header("UI")]
+    public GameObject gameOverPanel;
+    public Text title;
+
+    private bool _isGameEnded = false;
+    private bool isWon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+
+        for (int i = 0; i < birds.Count; i++)
+        {
+            birds[i].OnBirdDestroyed += ChangeBird;
+            birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        tapCollider.enabled = false;
+        slingShooter.InitiateBird(birds[0]);
+        _shotBird = birds[0];
+    }
+
+    private void Update()
+    {
+        if (_isGameEnded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    public void ChangeBird()
+    {
+        tapCollider.enabled = false;
+
+        if (_isGameEnded)
+        {
+            return;
+        }
+
+        birds.RemoveAt(0);
+
+        if (birds.Count > 0)
+        {
+            slingShooter.InitiateBird(birds[0]);
+            _shotBird = birds[0];
+        }
+
+        if (birds.Count == 0)
+        {
+            _isGameEnded = true;
+            isWon = false;
+            ShowGameOverUI(isWon);
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (enemies[i].gameObject == destroyedEnemy)
+            {
+                enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (enemies.Count == 0)
+        {
+            _isGameEnded = true;
+            isWon = true;
+            ShowGameOverUI(isWon);
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        trailController.SetBird(bird);
+        StartCoroutine(trailController.SpawnTrail());
+        tapCollider.enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+
+    private void ShowGameOverUI(bool isWon)
+    {
+        gameOverPanel.SetActive(true);
+
+        if (isWon)
+        {
+            title.text = "You Win!";
+        }
+        else
+        {
+            title.text = "You Lose!";
+        }
+    }
+}
---
+++ Assets/Scripts/SlingShooter.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShooter : MonoBehaviour
+{
+    public CircleCollider2D collider;
+    public LineRenderer trajectory;
+
+    private Vector2 _startPos;
+
+    [SerializeField] private float _radius = 0.75f;
+    [SerializeField] private float _throwSpeed = 30f;
+
+    private Bird _bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        collider.enabled = false;
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        float distance = Vector2.Distance(_startPos, transform.position);
+
+        _bird.Shoot(velocity, distance, _throwSpeed);
+
+        //Kembalikan ketapel ke posisi awal
+        gameObject.transform.position = _startPos;
+
+        trajectory.enabled = false;
+    }
+
+    public void InitiateBird(Bird bird)
+    {
+        _bird = bird;
+        _bird.MoveTo(gameObject.transform.position, gameObject);
+        collider.enabled = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        //Mengubah posisi mouse ke world position
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Hitung supaya 'karet' ketapel berada dalam radius yang ditentukan
+        Vector2 dir = pos - _startPos;
+        if (dir.sqrMagnitude > _radius)
+        {
+            dir = dir.normalized * _radius;
+
+        }
+        transform.position = _startPos + dir;
+
+        float distance = Vector2.Distance(_startPos, transform.position);
+        if (!trajectory.enabled)
+        {
+            trajectory.enabled = true;
+        }
+
+        DisplayTrajectory(distance);
+    }
+
+    private void DisplayTrajectory(float distance)
+    {
+        if (_bird == null)
+        {
+            return;
+        }
+
+        Vector2 velocity = _startPos - (Vector2)transform.position;
+        int segmentCount = 5;
+        Vector2[] segments = new Vector2[segmentCount];
+
+        //Posisi awal trajectory merupakan posisi mouse dari player saat ini
+        segments[0] = transform.position;
+
+        //Velocity awal
+        Vector2 segVelocity = velocity * distance * _throwSpeed;
+
+        for (int i = 0; i < segmentCount; i++)
+        {
+            float elapseTime = i * Time.fixedDeltaTime * 5;
+            segments[i] = segments[0] + segVelocity * elapseTime + 0.5f * Physics2D.gravity * Mathf.Pow(elapseTime, 2);
+        }
+
+        trajectory.positionCount = segmentCount;
+        for (int i = 0; i < segmentCount; i++)
+        {
+            trajectory.SetPosition(i, segments[i]);
+        }
+    }
+}
